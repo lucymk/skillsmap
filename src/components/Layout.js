3add70f93df4fda78feab266df744dea77fed957
lucymk <lucy.m.king92@gmail.com>
2023-11-06 17:24:45 +0800

    testing imagie processing

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,7 +2,13 @@
 
 import React from 'react'
 import { container } from './layout.module.css'
+import { StaticImage } from 'gatsby-plugin-image'
 
 export default function Layout({ children }) {
-  return <main className={`${container}`}>{children}</main>
+  return (
+    <main className={`${container}`}>
+      <StaticImage src="../assets/icons/chevronRight.svg" />
+      {children}
+    </main>
+  )
 }
