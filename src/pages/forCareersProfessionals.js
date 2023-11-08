d636bc93ac7fb8551667b404ae1e2dbc455bd280
lucymk <lucy.m.king92@gmail.com>
2023-11-08 13:32:48 +0800

    add basic query and data to each page

--- src/pages/forCareersProfessionals.js
+++ src/pages/forCareersProfessionals.js
@@ -1,12 +1,32 @@
-import * as React from "react"
+/* eslint-disable react/prop-types */
+import * as React from 'react'
+import { graphql } from 'gatsby'
 
-const ForCareersProfessionalsPage = () => {
-    return (
-        <main>
-            <h1>For Careers Professionals Page</h1>
-            <p>For Careers Professionals Text ...</p>
-        </main>
-    )
+const ForCareersProfessionalsPage = ({
+  data: {
+    allAirtable: { nodes: dataFromAirtable },
+  },
+}) => {
+  return (
+    <main>
+      <h1>For Careers Professionals Page</h1>
+      <p>For Careers Professionals Text ...</p>
+    </main>
+  )
 }
 
 export default ForCareersProfessionalsPage
+
+// queryName filters by table, see gatsby-config
+
+export const query = graphql`
+  query {
+    allAirtable(filter: { queryName: { eq: "PageContent" } }) {
+      nodes {
+        data {
+          Skill
+        }
+      }
+    }
+  }
+`
