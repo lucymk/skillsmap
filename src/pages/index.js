c9a35e4965a6853f6f0dc4f6f51a57f98044895f
lucymk <lucy.m.king92@gmail.com>
2023-11-15 13:42:28 +0800

    no message

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,75 +1,56 @@
 /* eslint-disable react/prop-types */
 import * as React from 'react'
-import { Link, graphql } from 'gatsby'
+import { Link } from 'gatsby'
 import '../../global.css'
 import Layout from '../components/layout'
-import {
-  SkillsButton,
-  SubjectTag,
-  SubjectButton,
-  ActionButton,
-  SkillsMapButton,
-} from '../components/buttons'
-import WorkplaceExamples from '../components/workplaceExamples'
-import ClusterCategory from '../components/clusterCategory'
-import NavDrawer from '../components/navDrawer'
-import {
-  Headline,
-  H1,
-  H2,
-  H3,
-  Subheadline,
-  Body,
-  Caption,
-} from '../components/shared'
+import { SkillsMapButton } from '../components/buttons'
+import { H1, Copy, BulletList } from '../components/shared'
 
-const IndexPage = ({
-  data: {
-    allAirtable: { nodes: dataFromAirtable },
-  },
-}) => {
+const IndexPage = () => {
   return (
     <Layout>
-      <SkillsButton>
-        Communicate appropriately, accurately and effectively
-      </SkillsButton>
-      <SubjectTag>Design and Technology</SubjectTag>
-
-      <ActionButton>Categories</ActionButton>
+      <H1>Welcome to SkillsMap</H1>
+      <Copy>
+        <BulletList>
+          <li>
+            Have you ever wondered what the subjects you study at school or
+            college have to do with work and your career?
+          </li>
+          <li>
+            Have you ever really enjoyed a subject at school or college but
+            wondered if there will be any future use for it in the workplace?
+          </li>
+          <li>
+            Have you ever wondered what value your qualifications have to
+            employers?
+          </li>
+        </BulletList>
+        <p style={{ margin: 'var(--spacing-m) 0 var(--spacing-m) 0' }}>
+          If you have ever asked yourself any of these questions, then
+          SkillsMap® is here to help you.
+        </p>
+        SkillsMap® shows you a selection of the transferable skills developed
+        in each of 35 different academic subjects typically taught in pre-18
+        education. These are the skills that employers are looking for. You can
+        also find the skills that different subjects have in common. Try
+        searching your favourite subjects and see what skills you are building.
+      </Copy>
       <SkillsMapButton />
-      <WorkplaceExamples
-        title="Workplace Examples"
-        body="some text some more text"
-      />
-      <ClusterCategory />
-      <Headline>Home Page</Headline>
-      <Link to="/skillsmapSearch">Skillsmap Search</Link>
-      <br />
-      <Link to="/forLearners">For Learners</Link>
-      <br />
-      <Link to="/forEducators">For Educators</Link>
-      <br />
-      <Link to="/forCareersProfessionals">For Careers Professionals</Link>
-      <br />
-      <Link to="/about">About</Link>
-      <br />
-      <p>Text...</p>
+      <Copy>
+        If you want to learn more about SkillsMap first, visit{' '}
+        <Link style={{ color: 'var(--blue)' }} to="/forLearners">
+          For learners
+        </Link>
+        . If you are an educator, you can find out more about using SkillsMap on
+        our{' '}
+        <Link style={{ color: 'var(--blue)' }} to="forEducators">
+          For Educators
+        </Link>{' '}
+        page.
+      </Copy>
     </Layout>
   )
 }
-
 export default IndexPage
 
-export const Head = () => <title>Home Page</title>
-
-export const query = graphql`
-  query {
-    allAirtable(filter: { queryName: { eq: "Skills" } }) {
-      nodes {
-        data {
-          Skill
-        }
-      }
-    }
-  }
-`
+// export const Head = () => <title>Home Page</title>
