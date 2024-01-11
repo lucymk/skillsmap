c6a8ebbeb6b2fa5b4e736931f06cc4aa59665917
lucymk <lucy.m.king92@gmail.com>
2024-01-11 10:58:08 +0530

    update Gatsby SSR title and lang

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,14 +1,26 @@
 import * as React from 'react'
 
-export const onRenderBody = ({ setHeadComponents }) => {
-  setHeadComponents([
-    <link
-      rel="preload"
-      href="/fonts/Inter.ttf"
-      as="font"
-      type="font/ttf"
-      crossOrigin="anonymous"
-      key="interFont"
-    />,
-  ])
+const HtmlAttributes = {
+  lang: 'en',
+}
+
+const HeadComponents = [
+  <link
+    rel="preload"
+    href="/fonts/Inter.ttf"
+    as="font"
+    type="font/ttf"
+    crossOrigin="anonymous"
+    key="interFont"
+  />,
+  // eslint-disable-next-line react/jsx-key
+  <title>
+    Your SkillsMap®: an application which helps you discover the transferable
+    skills you are building and using in your studies.
+  </title>,
+]
+
+export const onRenderBody = ({ setHeadComponents, setHtmlAttributes }) => {
+  setHeadComponents(HeadComponents)
+  setHtmlAttributes(HtmlAttributes)
 }
